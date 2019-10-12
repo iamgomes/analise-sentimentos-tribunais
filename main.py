8cec0a08bd8afd22e637c3ebde8e2d8737299347
WILLIAM GOMES PEREIRA JUNIOR <WGOMES@tce.mt.gov.br>
2019-10-12 00:33:51 -0400

    add geocoding

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #coding: utf-8
 
 import robots.twitter as robot
+import robots.geocode as geo
 from robots.watson import sentimentWatson, keywordsWatson
 import pandas as pd
 
@@ -20,26 +21,22 @@ def main():
     dataSet['user'] = [tweet['user'] for tweet in contentTweets]
     dataSet['text'] = [tweet['text'] for tweet in contentTweets]
     dataSet['text_sanitized'] = [tweet['text_sanitized'] for tweet in contentTweets]
-    dataSet['location'] = [tweet['location'] for tweet in contentTweets]
-    dataSet['coordinates'] = [tweet['coordinates'] for tweet in contentTweets]
     dataSet['followers_count'] = [tweet['followers_count'] for tweet in contentTweets]
     dataSet['verified'] = [tweet['verified'] for tweet in contentTweets]
     dataSet['created_at'] = [tweet['created_at'] for tweet in contentTweets]
-    tweets_place = []
-    for tweet in contentTweets:
-        if tweet['place']:
-            tweets_place.append(tweet['place']['full_name'])
-        else:
-            tweets_place.append('null')
-    dataSet['place'] = [i for i in tweets_place]
-    
+    dataSet['coordinates'] = [tweet['coordinates'] for tweet in contentTweets]
+    dataSet['location'] = [tweet['location'] for tweet in contentTweets]
+    dataSet['place'] = [tweet['place'] for tweet in contentTweets]
+    dataSet['lat'] = [geo.coordinates(tweet['location'])['lat'] for tweet in contentTweets]
+    dataSet['lng'] = [geo.coordinates(tweet['location'])['lng'] for tweet in contentTweets]
+
     print('\nAplicando Watson...')
     print('--SCORE--')
-    dataSet['score'] = [sentimentWatson(tweet['text_sanitized'])['score'] for tweet in contentTweets]
+    #dataSet['score'] = [sentimentWatson(tweet['text_sanitized'])['score'] for tweet in contentTweets]
     print('--SENTIMENTOS--')
-    dataSet['sentiment'] = [sentimentWatson(tweet['text_sanitized'])['label'] for tweet in contentTweets]
+    #dataSet['sentiment'] = [sentimentWatson(tweet['text_sanitized'])['label'] for tweet in contentTweets]
     print('--KEYWORDS--')
-    dataSet['keywords'] = [keywordsWatson(tweet['text_sanitized']) for tweet in contentTweets]
+    #dataSet['keywords'] = [keywordsWatson(tweet['text_sanitized']) for tweet in contentTweets]
 
     print('\nExportando Dataset em CSV...')
     dataSet.to_csv('dataSet.csv', sep=';')
