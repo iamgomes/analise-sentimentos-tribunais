4270c679ddc4c7cea45732d5dfd87e11012a194c
WILLIAM GOMES PEREIRA JUNIOR <WGOMES@tce.mt.gov.br>
2019-10-16 00:20:48 -0400

    code update

--- robots/geocode.py
+++ robots/geocode.py
@@ -5,6 +5,7 @@ import googlemaps
 from decouple import config
 import pandas as pd
 
+
 def coordinates(file):
 
     df = pd.read_csv(file, sep=';')
@@ -15,17 +16,17 @@ def coordinates(file):
 
     for i in range(len(df)):
         print('Buscando latitude e longitude de {}'.format(i))
-        geocode_result = gmaps_key.geocode(df.loc[i,'location'])
+        geocode_result = gmaps_key.geocode(df.loc[i, 'location'])
         try:
             lat = geocode_result[0]['geometry']['location']['lat']
             lng = geocode_result[0]['geometry']['location']['lng']
-            df.loc['lat'] = lat
-            df.loc['lng'] = lng
+            df.loc[i, 'lat'] = lat
+            df.loc[i, 'lng'] = lng
         except:
             lat = None
             lng = None
 
-    file_name = 'tweets_kml.cvs'
-    df.to_csv(file_name, sep=';', index=None)
+    file_name = 'tweets_kml.csv'
+    df.to_csv(file_name, sep=';')
 
     print('Arquivo {} gerado com sucesso!'.format(file_name))
